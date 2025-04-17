49887fe7105842ba320e103670e90df813ff5754
VAGNER OLIVEIRA <116770973+wildyhunter@users.noreply.github.com>
2025-04-17 12:54:38 +0100

    feat: create form in app.jsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import {Container} from './components/Container';
 import { Logo } from './components/Logo';
 import { Menu } from './components/Menu';
+import { CountDown } from './components/CountDown';
 
 import './style/Theme.css';
 import './style/Global.css';
@@ -15,6 +16,32 @@ export function App() {
 
             <Container>
                 <Menu />
+            </Container>
+
+            <Container>
+                <CountDown />
+            </Container>
+
+            <Container>
+                <form className="form" action="">
+                    <div className="formBox">
+                        <label htmlFor="meuInput">task</label>
+                        <input type="text" id="meuInput" placeholder="new task" />
+                    </div>
+
+                    <div className="formBox">
+                        <p>Lorem ipsum dolor sit amet.</p>
+                    </div>
+
+                    <div className="formBox">
+                        <p>Ciclos</p>
+                        <p>0 0 0 0 0</p>
+                    </div>
+
+                    <div className="formBox">
+                        <button>Enviar</button>
+                    </div>
+                </form>
             </Container>
         </>
     );
