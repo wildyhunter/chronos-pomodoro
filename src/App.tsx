601a3e26119892abb54ae9bc04c73330fdf4a562
Vagner Oliveira <wagneroliveiroficial@gmail.com>
2025-04-20 22:26:06 +0100

    fix: create children state

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,30 @@
 import { Home } from './pages/Home';
+import { useState } from 'react';
 
 import './style/Theme.css';
 import './style/Global.css';
+import { TaskStateModel } from './models/TaskStateModel';
+
+const initialState: TaskStateModel = {
+    tasks: [],
+    secondsRemaining: 0,
+    formattedSecondsRemaining: '',
+    activeTask: null,
+    currentCycle: 0,
+    config: {
+        WorkTime: 25,
+        ShortBreakTime: 5,
+        LongBreakTime:15,
+    },
+}
 
 export function App() {
+    const [state, setState] = useState(initialState);
+
+
     return (
         <>
-            <Home />
+            <Home state={state} setState={setState}/>
         </>
     );
 }
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -2,10 +2,18 @@ import { Container } from '../../components/Container';
 import { CountDown } from '../../components/CountDown';
 import { MainForm } from '../../components/MainForm';
 import { MainTemplate } from '../../components/Templates/MainTemplate';
+import { TaskStateModel } from '../../models/TaskStateModel';
 
 import styles from './styles.module.css';
 
-export function Home() {
+type Props = {
+    state: TaskStateModel
+    setState: React.Dispatch<React.SetStateAction<TaskStateModel>>
+};
+
+
+export function Home(props: Props) {
+    
     return (
         <MainTemplate>
             <Container>
