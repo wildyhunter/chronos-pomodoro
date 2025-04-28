3b42cf6d4cc671082c544f7a7ec3f82bb11ae50a
Vagner Oliveira <wagneroliveiroficial@gmail.com>
2025-04-28 23:04:38 +0100

    feat: create form settings page

--- src/pages/Settings/index.tsx
+++ src/pages/Settings/index.tsx
@@ -1,16 +1,56 @@
 import { Container } from '../../components/Container';
 import { Heading } from '../../components/Heading';
 import { MainTemplate } from '../../components/Templates/MainTemplate';
+import styles from '../../components/MainForm/styles.module.css';
+import { DefaultInput } from '../../components/DefaultInput';
+import { DefaultButton } from '../../components/DefaultButton';
+import { SaveIcon } from 'lucide-react';
 
 export function Settings() {
     return (
         <MainTemplate>
-            <Heading>Configurações</Heading>
+            <Container>
+                <Heading>Configurações</Heading>
+            </Container>
+
             <Container>
                 <p style={{ textAlign: 'center' }}>
                     Modifique as configurações para tempo de foco, descanso
                     curto e descanso longo.
                 </p>
+            </Container>
+
+            <Container>
+                <form action="" className={styles.form}>
+                    <div className={styles.formBox}>
+                        <DefaultInput
+                            id="workTime"
+                            type="number"
+                            labelText="Foco"
+                        />
+                    </div>
+                    <div className={styles.formBox}>
+                        <DefaultInput
+                            id="shortBreakTime"
+                            type="number"
+                            labelText="Descanso Curto"
+                        />
+                    </div>
+                    <div className={styles.formBox}>
+                        <DefaultInput
+                            id="workTime"
+                            type="number"
+                            labelText="Descanso Longo"
+                        />
+                    </div>
+                    <div className={styles.formBox}>
+                        <DefaultButton
+                            icon={<SaveIcon />}
+                            aria-label="Salvar Configurações"
+                            title="Salvar Configurações"
+                        />
+                    </div>
+                </form>
             </Container>
         </MainTemplate>
     );
